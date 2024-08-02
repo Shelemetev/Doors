9d7ffc439d4892888c645f116e221dd7f4696830
Egor <egor.shelemetev228@mail.ru>
2024-08-02 15:18:46 +1000

    fix

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('wheel', (event) => {
+    console.log(event.deltaY);
+    if (event.deltaY > 0) {
+      document.querySelector('.header').classList.remove('header--visible')
+    } else {
+      document.querySelector('.header').classList.add('header--visible')
+    }
+})
+// Работа header
