bdc883e6cbfe20b96594c90479f7e53d90d64a97
Egor <egor.shelemetev228@mail.ru>
2024-08-07 14:04:09 +1000

    fix

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-window.addEventListener('wheel', (event) => {
-    console.log(event.deltaY);
-    if (event.deltaY > 0) {
-      document.querySelector('.header').classList.remove('header--visible')
-    } else {
-      document.querySelector('.header').classList.add('header--visible')
-    }
-})
+// window.addEventListener('wheel', (event) => {
+//     console.log(event.deltaY);
+//     if (event.deltaY > 0) {
+//       document.querySelector('.header').classList.remove('header--visible')
+//     } else {
+//       document.querySelector('.header').classList.add('header--visible')
+//     }
+// })
 // Работа header
+
+document.addEventListener('scroll', function() {
+  const posTop = document.querySelector('.head').getBoundingClientRect().top;
+  
+  console.log(posTop);
+  // Блок целиком находится в видимой зоне
+  document.querySelector('.header').classList.toggle('header--visible', posTop + document.querySelector('.header').clientHeight <= window.innerHeight && posTop >= 0);
+});
